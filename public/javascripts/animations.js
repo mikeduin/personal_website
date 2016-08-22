1e927b50cd68646595a6f692a0976714bed6e68e
Mike Duin <michael.s.duin@gmail.com>
2016-08-22 06:47:31 -0700

    add ALA sidebar sliders

--- public/javascripts/animations.js
+++ public/javascripts/animations.js
@@ -3,7 +3,7 @@ angular
   .animation('.slideDown', function() {
     return {
   		addClass: function(element, className, done) {
-              jQuery(element).slideDown(done);
+        jQuery(element).slideDown(done);
   		},
   		removeClass: function(element, className, done) {
   			jQuery(element).slideUp(done);
