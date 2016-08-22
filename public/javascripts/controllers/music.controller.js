66e8423daceca6b7549dca96c131fafe37383575
Mike Duin <michael.s.duin@gmail.com>
2016-08-22 09:57:26 -0700

    reconfig from anchorscroll to rootscope scrolltop

--- public/javascripts/controllers/music.controller.js
+++ public/javascripts/controllers/music.controller.js
@@ -1,8 +1,14 @@
 angular
   .module('mySite')
-  .controller('MusicController', ['$scope', '$state', 'spotifyService', MusicController])
+  .controller('MusicController', ['$scope', '$state', '$anchorScroll', '$location', 'spotifyService', MusicController])
 
-function MusicController ($scope, $state, spotifyService) {
+function MusicController ($scope, $state, $anchorScroll, $location, spotifyService) {
   $scope.$state = $state;
 
+  $scope.vm.gotoId = function(id) {
+    var old = $location.hash();
+    $location.hash(id);
+    $anchorScroll();
+    $location.hash(old);
+  };
 }
