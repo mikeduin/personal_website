d77c72328a7d410852c6c9a9ccc6b8e9dd101e97
Mike Duin <michael.s.duin@gmail.com>
2017-03-08 00:06:21 -0800

    add game + year filters

--- public/javascripts/controllers/db.controller.js
+++ public/javascripts/controllers/db.controller.js
@@ -1,8 +1,8 @@
 angular
   .module('mySite')
-  .controller('DbController', ['dbService', DbController]);
+  .controller('DbController', ['dbService', '$scope', DbController]);
 
-function DbController (dbService) {
+function DbController (dbService, $scope) {
   var vm = this;
   vm.dbForm = {};
   vm.added = [];
@@ -10,9 +10,15 @@ function DbController (dbService) {
   vm.getEntrants = function() {
     dbService.getEntrants().then(function(res){
       vm.entrants = res;
-      console.log(vm.entrants);
+      vm.entrants.unshift({
+        name: "(all entrants)"
+      });
     })
   }
+
+  $scope.$watch('vm.entrants', function(){
+    vm.sel_entrant = vm.entrants[0];
+  })
 
   vm.addDbEntry = function() {
     dbService.addDbEntry(vm.dbForm).then(function(res){
@@ -27,5 +33,26 @@ function DbController (dbService) {
       })
       vm.dbForm = {};
     })
-  }
+  };
+
+  vm.years = [2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001];
+
+  vm.games = [
+    'Fantasy MLB',
+    'Fantasy NBA',
+    'Fantasy NBA CDL',
+    'Fantasy NFL',
+    'FIFA World Cup Battle the Bookies',
+    'FIFA World Cup Bracket Pool',
+    'FIFA World Cup Survivor',
+    'NBA All-Star Spectacular',
+    'NBA Playoffs',
+    'NFL Battle the Bookies',
+    'NFL Confidence',
+    'NFL Survivor',
+    'NCAA March Madness',
+    "NCAAF Pick'em'",
+    "NCAAF Bowl Pick'em",
+    'PGA Majors'
+  ];
 }
