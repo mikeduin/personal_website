1fea5dee4e4f9bb5a0fd1e14d04e7c23fb0b1aff
Mike Duin <michael.s.duin@gmail.com>
2017-03-08 04:14:01 -0800

    build out user data calcs

--- routes/db.js
+++ routes/db.js
@@ -17,7 +17,7 @@ router.get('/entrants', function(req, res, next){
   })
 })
 
-// THIS FUNCTION BELOW ADMITTEDLY REALLY, TOTALLY SUCKS WITH SIX DIFFERENT POTENTIAL CALLS. BUT HOW DO I SET 'ALL' VALUES DYNAMICALLY IF A FILTER IS NOT SELECTED?
+// THIS FUNCTION BELOW ADMITTEDLY TOTALLY SUCKS WITH SEVEN DIFFERENT POTENTIAL CALLS. NEED TO REDUCE. BUT HOW DO I SET 'ALL' VALUES DYNAMICALLY IN A POSTGRES where() QUERY IF A FILTER IS NOT SELECTED?
 
 router.get('/query/:entrant/:game/:year', function(req, res, next){
   var entrant, game, year;
@@ -31,7 +31,50 @@ router.get('/query/:entrant/:game/:year', function(req, res, next){
     User_Results().where({
       name: req.params.entrant
     }).then(function(results){
-      res.json(results);
+      var userData = {};
+      userData['entries'] = results.length;
+      userData['uniqueGms'] = [];
+      userData['titles'] = 0;
+      userData['podiums'] = 0;
+      userData['buyins'] = 0;
+      userData['prizes'] = 0;
+      userData['profits'] = 0;
+      userData['debut'] = new Date().getFullYear();
+      userData['active'] = results[0].season;
+      userData['bigCash'] = 0;
+      userData['bigCashGame'] = null;
+      userData['bigCashYear'] = null;
+      for (var i=0; i<results.length; i++) {
+        if (userData['uniqueGms'].indexOf(results[i]) === -1) {
+          userData['uniqueGms'].push(results[i])
+        };
+        if (results[i].rank !== null && results[i].rank < 2) {
+          userData['titles']++;
+        };
+        if (results[i].rank !== null && results[i].rank < 4) {
+          userData['podiums']++;
+        };
+        if (results[i].season < userData['debut']) {
+          userData['debut'] = results[i].season;
+        };
+        if (results[i].season > userData['active']) {
+          userData['active'] = results[i].season;
+        };
+        if (results[i].prize > userData['bigCash']) {
+          userData['bigCash'] = results[i].prize;
+          userData['bigCashGame'] = results[i].game;
+          userData['bigCashYear'] = results[i].season;
+        };
+        userData['buyins'] += results[i].buyin;
+        userData['prizes'] += results[i].prize;
+        userData['profits'] += results[i].profit;
+      };
+      userData['ROI'] = userData['profits'] / userData['buyins'];
+      userData['uniqueGmsTotal'] = userData['uniqueGms'].length;
+      res.json({
+        results: results,
+        userData: userData
+      });
     })
   } else if (req.params.entrant === '*' && req.params.game !== '*' && req.params.year === '*') {
     User_Results().where({
