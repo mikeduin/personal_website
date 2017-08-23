cacd03b6c5184e118d221b85513c643646d0cb76
Mike Duin <michael.s.duin@gmail.com>
2017-08-23 01:34:28 -0700

    update buyins + march madness yoy prog

--- public/javascripts/controllers/ala.controller.js
+++ public/javascripts/controllers/ala.controller.js
@@ -390,7 +390,7 @@ function AlaController ($scope, $anchorScroll, $location, alaService, $state, $s
       }
     },
     'scaleX':{
-      'values': [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
+      'values': [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
       'offset-y': 4,
     },
     'scaleY':{
@@ -406,7 +406,7 @@ function AlaController ($scope, $anchorScroll, $location, alaService, $state, $s
     },
     'series':[
       {
-        "values": [660, 1134, 1510, 1680, 2110, 3540, 5423, 6511, 6675],
+        "values": [660, 1134, 1510, 1680, 2110, 3540, 5423, 6511, 6675, 8787],
         "type": 'line',
         "line-color": "#DC5623",
         'legend-text': 'Prize $',
