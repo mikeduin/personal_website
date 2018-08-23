38a882a86eb2195f44e638a0856157e359e49769
Mike Duin <michael.s.duin@gmail.com>
2018-08-22 23:54:23 -0700

    update survivor for 2017 results

--- public/javascripts/controllers/ala.controller.js
+++ public/javascripts/controllers/ala.controller.js
@@ -667,7 +667,7 @@ function AlaController ($scope, $anchorScroll, $location, alaService, authServic
       }
     },
     'scaleX':{
-      'values': [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
+      'values': [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
       'offset-y': 4,
     },
     'scaleY':{
@@ -683,7 +683,7 @@ function AlaController ($scope, $anchorScroll, $location, alaService, authServic
     },
     'series':[
       {
-        "values": [1155, 1870, 1870, 2750, 3905, 4180, 4620, 4950, 3880],
+        "values": [1155, 1870, 1870, 2750, 3905, 4180, 4620, 4950, 3880, 3800],
         "type": 'line',
         "line-color": "#DC5623",
         'legend-text': 'Prize $',
