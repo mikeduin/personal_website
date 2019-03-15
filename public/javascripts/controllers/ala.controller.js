e4dbb6fd6889c6b6cadf62225fbd134b9dfd83a7
Mike Duin <michael.s.duin@gmail.com>
2019-03-15 04:46:16 -0700

    update march madness

--- public/javascripts/controllers/ala.controller.js
+++ public/javascripts/controllers/ala.controller.js
@@ -453,7 +453,7 @@ function AlaController ($scope, $anchorScroll, $location, alaService, authServic
       }
     },
     'scaleX':{
-      'values': [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+      'values': [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018],
       'offset-y': 4,
     },
     'scaleY':{
@@ -469,7 +469,7 @@ function AlaController ($scope, $anchorScroll, $location, alaService, authServic
     },
     'series':[
       {
-        "values": [660, 1134, 1510, 1680, 2110, 3540, 5423, 6511, 6675, 8787],
+        "values": [660, 1134, 1510, 1680, 2110, 3540, 5423, 6511, 6675, 8787, 9792],
         "type": 'line',
         "line-color": "#DC5623",
         'legend-text': 'Prize $',
