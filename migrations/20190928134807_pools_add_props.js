e76ba7a69a16c0d7a5f63b58fdbed3a25698f8bd
Mike Duin <michael.s.duin@gmail.com>
2019-09-28 19:28:04 -0700

    modify pools migration

--- migrations/20190928134807_pools_add_props.js
+++ migrations/20190928134807_pools_add_props.js
@@ -1,11 +1,17 @@
 exports.up = function(knex, Promise) {
   return knex.schema.alterTable('pools', (t) => {
-    t.specificType('props', 'jsonb[]');
+    t.boolean('bonus_active');
+    t.integer('buyin_min');
+    t.integer('lives');
+    t.integer('rebuy_price');
   })
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.alterTable('pools', (t) => {
-    t.dropColumn('props');
+    t.dropColumn('bonus_active');
+    t.dropColumn('buyin_min');
+    t.dropColumn('lives');
+    t.dropColumn('rebuy_price');
   })
 };
