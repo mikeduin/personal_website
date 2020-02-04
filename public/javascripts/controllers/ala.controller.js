07e569b20d3b76b6b197eceb80e5a7af42e98dc1
Mike Duin <michael.s.duin@gmail.com>
2020-02-04 03:56:29 -0800

    update bowl pick em for 2019-20

--- public/javascripts/controllers/ala.controller.js
+++ public/javascripts/controllers/ala.controller.js
@@ -741,7 +741,7 @@ function AlaController ($scope, $anchorScroll, $location, alaService, authServic
       }
     },
     'scaleX':{
-      'values': [2009, 2010, 2012, 2013, 2014, 2015, 2016, 2017, 2018],
+      'values': [2009, 2010, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
       'offset-y': 4,
     },
     'scaleY':{
@@ -757,7 +757,7 @@ function AlaController ($scope, $anchorScroll, $location, alaService, authServic
     },
     'series':[
       {
-        "values": [100, 110, 165, 320, 600, 920, 800, 920, 1360],
+        "values": [100, 110, 165, 320, 600, 920, 800, 920, 1360, 1470],
         "type": 'line',
         "line-color": "#DC5623",
         'legend-text': 'Prize $',
