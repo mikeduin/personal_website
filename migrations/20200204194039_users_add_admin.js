ec7fa0c3146de0990dde63d78e2b91e90dd4d45b
Mike Duin <michael.s.duin@gmail.com>
2020-02-04 19:45:49 -0800

    fix migration bug

--- migrations/20200204194039_users_add_admin.js
+++ migrations/20200204194039_users_add_admin.js
@@ -1,6 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.alterTable('users', (t) => {
     t.boolean('admin').defaultTo('false');
+  })
 };
 
 exports.down = function(knex, Promise) {
