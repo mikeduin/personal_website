9a108b130e5c9108411908d9f429a0574e64eccc
Michael Duin <michael.s.duin@gmail.com>
2020-11-09 15:37:23 -0800

    convert blog from mongo to psql

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,18 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    // connection: process.env.DATABASE_URL
+    connection: process.env.TEST_DB
   },
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    // connection: process.env.DATABASE_URL
+    connection: process.env.TEST_DB
   }
 
 };
---
+++ migrations/20201011233138_beers.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('beers', t => {
+    t.increments();
+    t.string('name');
+    t.string('beername');
+    t.string('brewery');
+    t.string('style');
+    t.integer('abv');
+    t.integer('quantity');
+    t.boolean('cold');
+    t.string('image');
+    t.integer('price');
+    t.string('size');
+    t.timestamp('ordered');
+    t.text('description');
+    t.timestamp('updated_at');
+    t.timestamp('created_at');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('beers');
+};
