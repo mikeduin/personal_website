9536f7212d67815ba4251cca595a3292370a072a
Michael Duin <michael.s.duin@gmail.com>
2022-03-07 23:03:33 -0800

    update btb 2021

--- public/javascripts/controllers/ala.controller.js
+++ public/javascripts/controllers/ala.controller.js
@@ -914,7 +914,7 @@ function AlaController ($scope, $anchorScroll, $location, alaService, authServic
       }
     },
     'scaleX':{
-      'values': [2014, 2015, 2016, 2017, 2018, 2019, 2020],
+      'values': [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
       'offset-y': 4,
     },
     'scaleY':{
@@ -930,7 +930,7 @@ function AlaController ($scope, $anchorScroll, $location, alaService, authServic
     },
     'series':[
       {
-        "values": [1620, 3600, 5100, 4890, 5580, 6634, 8330],
+        "values": [1620, 3600, 5100, 4890, 5580, 6634, 8330, 8921],
         "type": 'line',
         "line-color": "#DC5623",
         'legend-text': 'Prize $',
