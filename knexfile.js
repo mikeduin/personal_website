bd23dc9f48f26d2b16164a1cc1ae83f801696929
Michael Duin <michael.s.duin@gmail.com>
2022-11-15 03:36:57 -0800

    update knex

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,28 @@
 require('dotenv').config();
+const pg = require('pg')
+pg.defaults.ssl = {
+   rejectUnauthorized: false,
+}
 
-const parse = require("pg-connection-string").parse;
-const pgconfigProd = parse(process.env.DATABASE_URL);
-const pgconfigDev = parse(process.env.TEST_DB_URL);
-// const pgconfigDev = parse(process.env.DATABASE_URL);
+// const parse = require("pg-connection-string").parse;
+// const pgconfig = parse(process.env.TEST_DB_URL);
 
-// Add SSL setting to default environment variable
-pgconfigProd.ssl = { rejectUnauthorized: false };
-pgconfigDev.ssl = { rejectUnauthorized: false };
+// console.log(process.env);
 
 module.exports = {
   development: {
     client: 'pg',
-    connection: pgconfigDev
+    // connection: process.env.DATABASE_URL,
+    connection: process.env.TEST_DB_URL,
+    // connection: pgconfig,
+    ssl: {
+      rejectUnauthorized: false,  
+    },
+    ssl: 'no-verify' // try this if above does not work for you
   },
 
   production: {
     client: 'pg',
-    connection: pgconfigProd
+    connection: process.env.DATABASE_URL
   }
 };
