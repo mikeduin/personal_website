6f2c1581dbda4bca800f374fa2df3a8fed51f772
Michael Duin <michael.s.duin@gmail.com>
2022-11-15 01:50:33 -0800

    reset knex

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const parse = require("pg-connection-string").parse;
 const pgconfigProd = parse(process.env.DATABASE_URL);
-// const pgconfigDev = parse(process.env.TEST_DB_URL);
-const pgconfigDev = parse(process.env.DATABASE_URL);
+const pgconfigDev = parse(process.env.TEST_DB_URL);
+// const pgconfigDev = parse(process.env.DATABASE_URL);
 
 // Add SSL setting to default environment variable
 pgconfigProd.ssl = { rejectUnauthorized: false };
